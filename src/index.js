81f6416d0f21a0119b43564c62160dafda859f82
Alejandro Bernal <alejandro.bernal@urbantxt.com>
2022-03-17 21:33:34 -0700

    wrote build file

--- src/index.js
+++ src/index.js
@@ -1,32 +1,28 @@
+import Phaser from "phaser";
 
-import Phaser from 'Phaser'
+import Game from "./scenes/Game.js";
+import GameOver from "./scenes/GameOver.js";
 
-import Game from './scenes/Game.js'
-import GameOver from './scenes/GameOver.js'
-
-import './assets/scss/index.scss'
+import "./assets/scss/index.scss";
 
 const config = {
-    width: 640,
-    height: 480,
-    parent: 'mario',
-    backgroundColor: '#FFFFAC',
-    title: 'Tilemap',
-    url: 'webtips.dev',
-    pixelArt: true,
-    physics: {
-        default: 'arcade',
-        arcade: {
-            // debug: true, // Set it to true if you want debugger enabled by default
-            gravity: {
-                y: 1000
-            }
-        }
+  width: 640,
+  height: 480,
+  parent: "mario",
+  backgroundColor: "#FFFFAC",
+  title: "Tilemap",
+  url: "webtips.dev",
+  pixelArt: true,
+  physics: {
+    default: "arcade",
+    arcade: {
+      // debug: true, // Set it to true if you want debugger enabled by default
+      gravity: {
+        y: 1000,
+      },
     },
-    scene: [
-        Game,
-        GameOver
-    ]
+  },
+  scene: [Game, GameOver],
 };
 
 new Phaser.Game(config);
